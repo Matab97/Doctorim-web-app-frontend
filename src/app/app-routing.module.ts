bacc97ec81bee67d8506c71ec6bb30661fca1ffd
Sidaty1 <Sidaty.elhadrami@yahoo.com>
2021-04-26 07:13:56 +0200

    Login page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+    {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+    {path: 'statistics', component: StatisticsComponent, canActivate: [AuthGuard]},
+    {path: '', component: LoginComponent}
+    
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
