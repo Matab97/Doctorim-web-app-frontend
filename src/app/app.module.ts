7ef4623f49fdf88f9d81433e79755b1f01bc09c4
matab97 <medabbad0@gmail.com>
2021-04-30 02:46:13 +0100

    login and register pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { ChoosingComponent } from './components/choosing/choosing.component';
+import { DoctorregisterComponent } from './components/doctorregister/doctorregister.component';
+import { PatientregisterComponent } from './components/patientregister/patientregister.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    StatisticsComponent,
+    ChoosingComponent,
+    DoctorregisterComponent,
+    PatientregisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
